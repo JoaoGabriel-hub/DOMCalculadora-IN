ec03701ddf3604293aa51a404d708d9f11104fcb
João Gabriel Ramos <jgbvramos@id.uff.br>
2024-08-01 15:35:29 -0300

    feat: restringindo notas inválidas

--- main.js
+++ main.js
@@ -1,12 +1,23 @@
+let contador = 1;
+
 function enviar_nota() {
     let input = document.querySelector(".input");
+
+    let nota = parseFloat(input.value);
+
+    if (nota < 0 || nota > 10) {
+        alert("Nota inválida. As notas devem ser de 0 até 10");
+        return;
+    }
+
     let texto = document.createElement("p");
-    texto.innerText = input.value;
+    texto.innerText = `A nota ${contador} é ${input.value}`; 
 
     let secao = document.querySelector(".espaco");
     secao.append(texto);
 
     input.value = "";
+    contador++;
 }    
 
 let btn_adc = document.querySelector(".btn_adc");
